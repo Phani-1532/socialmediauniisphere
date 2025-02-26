93a56979c5242b33df4e6ee5ffdecd9fee14e50d
Phanindra <phanindragadireddy2@gmail.com>
2025-02-26 09:28:59 +0530

    feed added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
-import Home from '../../assets/headerImages/Home.svg'
-import Plus from '../../assets/headerImages/plusIcon.svg'
-import Network from '../../assets/headerImages/networkIcon.svg'
-import Calender from '../../assets/headerImages/calenderIcon.svg'
-import Notification from '../../assets/headerImages/notificationsIcon.svg'
+import Home from '../../assets/headerImages/Home.svg';
+import Plus from '../../assets/headerImages/plusIcon.svg';
+import Network from '../../assets/headerImages/networkIcon.svg';
+import Calender from '../../assets/headerImages/calenderIcon.svg';
+import Notification from '../../assets/headerImages/notificationsIcon.svg';
 
 const Footer = () => {
+  const [activeIcon, setActiveIcon] = useState("home");
+
+  const handleIconClick = (iconName) => {
+    setActiveIcon(iconName);
+  };
+
   return (
     <div className="footer">
+      <div
+        className={`footer-icon ${activeIcon === "home" ? "active" : ""}`}
+        onClick={() => handleIconClick("home")}
+      >
         <img src={Home} alt="Home" />
-        <img src={Notification} alt='Notifications' />
+      </div>
+      <div
+        className={`footer-icon ${activeIcon === "notification" ? "active" : ""}`}
+        onClick={() => handleIconClick("notification")}
+      >
+        <img src={Notification} alt="Notifications" />
+      </div>
+      <div
+        className={`footer-icon ${activeIcon === "add" ? "active" : ""}`}
+        onClick={() => handleIconClick("add")}
+      >
         <img src={Plus} alt="Add" />
+      </div>
+      <div
+        className={`footer-icon ${activeIcon === "calendar" ? "active" : ""}`}
+        onClick={() => handleIconClick("calendar")}
+      >
         <img src={Calender} alt="Calendar" />
+      </div>
+      <div
+        className={`footer-icon ${activeIcon === "network" ? "active" : ""}`}
+        onClick={() => handleIconClick("network")}
+      >
         <img src={Network} alt="Network" />
+      </div>
     </div>
   );
 };
--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -9,7 +9,17 @@ const Post = ({ user, profilePic, time, work, image, caption, likes }) => {
         <div>
           <h4 className="user-name">
             {user} <span className="time">{time} ago</span>
-            <button className="connect-btn">+Connect</button>
+            <button className="connect-btn">
+            <span className="color1">+</span>
+            <span> </span>
+        <span className="color2">C</span>
+        <span className="color3">O</span>
+        <span className="color4">N</span>
+        <span className="color5">N</span>
+        <span className="color6">E</span>
+        <span className="color7">C</span>
+        <span className="color8">T</span>
+            </button>
           </h4>
           <p className="user-info">{work}</p>
         </div>
@@ -21,9 +31,11 @@ const Post = ({ user, profilePic, time, work, image, caption, likes }) => {
       <div className="post-footer">
         <span className="likes-count">{likes} Likes</span>
         <div className="post-actions">
-          <span>❤️</span>
-          <span>💬</span>
-          <span>🔄</span>
+      
+        <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/share.png" alt="share"/>
+        <img width="24" height="24" src="https://img.icons8.com/ios/50/speech-bubble--v1.png" alt="speech-bubble--v1"/>
+        <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/filled-like.png" alt="filled-like"/>
+         
         </div>
       </div>
 
